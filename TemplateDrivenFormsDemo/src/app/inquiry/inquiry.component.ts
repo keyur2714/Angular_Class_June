43b8faea38fef1f3989a466a45a9357d0cae512d
keyur2714 <keyur.555kn@gmail.com>
2018-09-01 14:36:51 +0530

    Updated Template Driven Forms

--- TemplateDrivenFormsDemo/src/app/inquiry/inquiry.component.ts
+++ TemplateDrivenFormsDemo/src/app/inquiry/inquiry.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Inquiry } from './inquiry.model';
 @Component({
   selector: 'app-inquiry',
   templateUrl: './inquiry.component.html',
@@ -7,19 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InquiryComponent implements OnInit {
 
-  list: string[]=["A","B","C"];
+  inquiry : Inquiry = new Inquiry();
+  isSubmitted : boolean = false;
+
+  
+
+  avalibityList: string[]=[
+    "MON","THE","WED","THURSE","FRI","SAT","SUN"
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    this.inquiry.name = "keyur";
+    this.inquiry.availibity = this.avalibityList;
   }
 
   save(frm):void{
     console.log(frm);
-    console.log(frm.value);
+    console.log(frm.value);    
+    if(frm.valid){
+      this.isSubmitted = true;
+      console.log(this.inquiry);    
+    }
   }
 
   reset(frm){
+    this.inquiry = new Inquiry();
     frm.reset();
   }
 }
---
+++ TemplateDrivenFormsDemo/src/app/inquiry/inquiry.model.ts
@@ -0,0 +1,3 @@
+export class Inquiry{
+    constructor(public course?:string,public name?:string,public mobileNo?:string,public email?:string,public gender?:string,public availibity?:string[]){}
+}
